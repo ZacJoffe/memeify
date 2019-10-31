3d8e5dfe9adc4080c10b039c69e68cc7cb437529
Zac Joffe <zacharyjoffe@gmail.com>
2019-10-31 00:20:43 -0400

    Function for meme generation

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", meme_generator(String::from("meme")));
 }
 
-fn stringify(s: String) -> String {
+fn meme_generator(s: String) -> String {
+    let mut meme: String = String::from("");
 
+    for c in s.chars() {
+        meme.push(c);
+        meme.push(' ');
+    }
+
+    String::from(&meme[..meme.len() - 1])
 }
